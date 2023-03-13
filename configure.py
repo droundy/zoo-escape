1c9f450d066da24f25d4e53b028461037f254cb0
David Roundy <daveroundy@gmail.com>
2023-03-13 09:24:19 -0700

    more pieces

--- configure.py
+++ configure.py
@@ -1,12 +1,22 @@
 import glob
 
-for gif in glob.glob("*-cards/*.gif"):
-    base = gif[:-4]
-    print(f"| convert {gif}[0] -gravity center -extent 600x825 {base}.png")
+for json in glob.glob("*-cards/*.json"):
+    base = json[:-5]
+    print(f"| color-splotch --mini-deck --gif --path {base}.json")
+    print(f"< {base}.json")
+    print(f"> {base}.gif")
+    print(
+        f"| convert {base}.gif[0] -gravity center -extent 600x825 {base}.png")
+    print(f"< {base}.gif")
     print(f"> {base}.png")
 
 
-for gif in glob.glob("pieces/*.gif"):
-    base = gif[:-4]
-    print(f"| convert {gif}[0] -gravity center -extent 375x375 {base}.png")
+for json in glob.glob("pieces/*.json"):
+    base = json[:-5]
+    print(f"| color-splotch --large-circle-chit --gif --path {base}.json")
+    print(f"< {base}.json")
+    print(f"> {base}.gif")
+    print(
+        f"| convert {base}.gif[0] -gravity center -extent 375x375 {base}.png")
+    print(f"< {base}.gif")
     print(f"> {base}.png")
